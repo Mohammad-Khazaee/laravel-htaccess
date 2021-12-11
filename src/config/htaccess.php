76948a66d64fb15cde0800a36f7555c1f977bc98
mohammadkhazaee <mohammadkhazaei7410>
2021-12-11 17:35:19 +0330

    add config and stubs

---
+++ src/config/htaccess.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+   /*
+    |--------------------------------------------------------------------------
+    | public folder
+    |--------------------------------------------------------------------------
+    | 
+    | this is your app public folder in laravel by default is public of root 
+    | of the project in production it might change
+    |
+    */
+
+    "public_folder" => "public/"
+];
